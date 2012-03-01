95f2ff4c3f5509c512a8bbd9085061c8e39fc21c
Michel de Graaf <michel@re-invention.nl>
2012-03-01 21:25:36 +0100

    added ringr call class

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ end
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.2'
-
+gem "twilio-ruby"
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 gem 'haml', '~> 3.1.4'
---
+++ lib/ringr.rb
@@ -0,0 +1,19 @@
+require 'twilio-ruby'
+
+class Ringr
+  def initialize
+      account_sid = 'AC94571850a65e4706bb71b21cd9c6831a'
+      auth_token = '0cb6a2e4127d2cdca4c51fcad1dfd925'
+     @client =   Twilio::REST::Client.new account_sid, auth_token
+  end
+  def call(number,name)
+       @call = @client.account.calls.create(
+          :from => '+442033222875',
+          :to => number,
+          :url => "https://raw.github.com/gist/1952696/c612857033f5f385c53a9f77ad90f9fcaa41c331/test.xml"
+         )
+  end
+end
+
+x = Ringr.new
+x.call("+31648121608")
