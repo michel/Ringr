8dec49ef2923917c4726addfdac878cfb82f3772
Michel de Graaf <michel@re-invention.nl>
2012-03-01 21:37:56 +0100

    WIP

--- voip/testxml.rb
+++ voip/testxml.rb
@@ -3,6 +3,6 @@
 
 # build up a response
  response = Twilio::TwiML::Response.new do |r|
-   r.Say 'hello there', :voice => 'woman'
+   r.play 'http://izerion.com/da1392f0-460b-012f-74be-3c07544054a5.mp3'
 end
 puts response.text
