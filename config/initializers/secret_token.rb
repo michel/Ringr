d7062fe6e870fbb931d4c00b0a05763e56f5869a
Ralph Rooding <ralph.rooding@kabisa.nl>
2012-03-20 19:46:27 +0100

    Plain rails 3.2.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rrrrr::Application.config.secret_token = 'ed6513a33f6a8129c9af4b464f80009f09a7a3be48b6fa2d206200b5c58320d8cc6ea64a94c6c4a4c21de0c0ead933fde20956e0442ab8af5f94fc10240a9ccd'
